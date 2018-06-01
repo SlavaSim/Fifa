f7674e6613fbf80499c9ffb617faee34ed224e84
SlavaSim <slava.sim@gmail.com>
2018-06-01 09:33:03 +0300

    hide junk matches

--- buy_one.js
+++ buy_one.js
@@ -1,7 +1,13 @@
-$("select[data-resource-id='ChooseQuantity']")[0].selectedIndex = 1;
-setTimeout(function () {
-    $("select[data-resource-id='ChooseQuantity']").change();
-    setTimeout(function () {
-        $("button[data-resource-id='AddToShoppingBasket']").click();
-    }, 500);
-}, 100);
+// $("select[data-resource-id='ChooseQuantity']")[0].selectedIndex = 4;
+// debugger;
+// angular.element("select[data-resource-id='ChooseQuantity']").trigger("change");
+// angular.element("[data-resource-id=AddToShoppingBasket]").trigger("click");
+//
+//
+// $("select[data-resource-id='ChooseQuantity']")[0].selectedIndex = 1;
+// setTimeout(function () {
+//     $("select[data-resource-id='ChooseQuantity']").change();
+//     setTimeout(function () {
+//         $("button[data-resource-id='AddToShoppingBasket']").click();
+//     }, 500);
+// }, 100);
